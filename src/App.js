c6b489a0e70fe5b935825076487e234bc78110cd
Ronald Arias <ariasron@hotmail.com>
2021-08-24 10:35:30 -0500

    Basics of styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: green;
+  color: white;
+`;
+
+const Container = styled.div`
+  background: red;
+  color: white;
+  font-size: 2rem;
+  .hero {
+    background: blue;
+    font-size: 1.3rem;
+  }
+`;
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <div>
+      <h4>Comfy sloth starter</h4>
+      <Button>Click me</Button>
+      <Container>
+        <div>
+          <h3>Hello world</h3>
+        </div>
+        <div className="hero">Hero class</div>
+      </Container>
+    </div>
+  );
 }
 
-export default App
+export default App;
