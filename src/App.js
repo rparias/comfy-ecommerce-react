1596fd50d353290c4bddca011f01adde087f5f25
Ronald Arias <ariasron@hotmail.com>
2021-08-24 11:15:34 -0500

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Navbar, Sidebar, Footer } from "./components";
-import TestingStyledComponents from "./TestingStyledComponents";
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  Home,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+} from "./pages";
 
 function App() {
   return (
-    <div>
-      Comfy Sloth Starter
-      <TestingStyledComponents />
-    </div>
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProduct />} />
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
