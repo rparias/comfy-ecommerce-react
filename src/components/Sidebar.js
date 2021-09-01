62b3d4137340b9392d19b0bbcfbdd76c1867e737
Ronald Arias <ariasron@hotmail.com>
2021-08-31 21:29:10 -0500

    Toggles on/off the sidebar navigation menu

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,16 +9,14 @@ import CartButtons from "./CartButtons";
 import { useUserContext } from "../context/user_context";
 
 const Sidebar = () => {
-  const dataFromContext = useProductsContext();
-  console.log(dataFromContext);
+  const { isSidebarOpen, closeSidebar } = useProductsContext();
 
-  const isOpen = false;
   return (
     <SidebarContainer>
-      <aside className={`sidebar ${isOpen && "show-sidebar"}`}>
+      <aside className={`sidebar ${isSidebarOpen && "show-sidebar"}`}>
         <div className="sidebar-header">
           <img src={logo} alt="comfy sloth" />
-          <button className="close-btn" type="button">
+          <button className="close-btn" type="button" onClick={closeSidebar}>
             <FaTimes />
           </button>
         </div>
@@ -26,12 +24,16 @@ const Sidebar = () => {
           {links.map(({ id, text, url }) => {
             return (
               <li key={id}>
-                <Link to={url}>{text}</Link>
+                <Link to={url} onClick={closeSidebar}>
+                  {text}
+                </Link>
               </li>
             );
           })}
           <li>
-            <Link to="/checkout">checkout</Link>
+            <Link to="/checkout" onClick={closeSidebar}>
+              checkout
+            </Link>
           </li>
         </ul>
         <CartButtons />
